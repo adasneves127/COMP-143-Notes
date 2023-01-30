d281beb7f1a1375a59ded868d55f6960f18f8945
Alex Dasneves <63429791+adasneves127@users.noreply.github.com>
2023-01-29 20:30:12 -0500

    Update main.py
    Week 2 — 301 Note Example

---
+++ 301/Week02/main.py
@@ -0,0 +1,22 @@
+#Recall that we can create variables with the following syntax:
+num = 3
+
+#  What is the data currently contained within "num"?
+
+# What if i say:
+
+num = num + 5
+
+# What is in num now?
+
+#if i say:
+print(num)
+#What do we expect to happen?
+
+#How about with:
+print(num/2)
+
+#What will print out? What will num equal now?
+
+#Do you understand why this happens?
+
