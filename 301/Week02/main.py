37e839bafda3275c9bbf1d038d12220e3666f3f1
Alex Dasneves <adasneves127@gmail.com>
2023-01-29 22:40:46 -0500

    Created Topic Outline for 301

--- 301/Week02/main.py
+++ 301/Week02/main.py
@@ -20,3 +20,18 @@ print(num/2)
 
 #Do you understand why this happens?
 
+
+
+# Basic Data Types: String, Integer, Float, and Boolean
+
+# String: A string is just text. It can be any length, and can contain 0-9, A-Z, a-z, any symbols, and some other special characters. It is denoted by: "string", 'string', or """string"""
+
+# Integer: An integer is a whole number, either positive or negative. Examples include: 0, 10, -2, 102381, -1238912, etc.
+
+# Float: A float is any number, either positive or negative, that has a decimal point. Examples include: 0.0, 10.0, -2.0, 102381.0, -1238912.0, PI, etc.
+
+# Boolean: A Boolean is a logic value. It can only be True or False (There is no in-between state). For now, we will stick with defining it as True or False.
+
+
+# Python Courtesy: https://www.python.org/dev/peps/pep-0008/ 
+# Pep is the "Python Enhancement Proposal" and is a document that outlines the style guidelines for Python code. It is a good idea to follow these guidelines, as it makes your code easier to read and understand.
---
+++ 303/Week01/main.py
@@ -0,0 +1,8 @@
+num = 10
+
+while True:
+    if num >= 10:
+        num //= 2
+    else:
+        num *= 3
+    print(num)
