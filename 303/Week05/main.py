d01e450e4fe080ac10d2a992e3344e0099a7a711
Alex Dasneves <adasneves127@gmail.com>
2023-02-18 14:52:32 -0500

    Weekly Notes Posted

---
+++ 303/Week05/main.py
@@ -0,0 +1,36 @@
+# Problem 1:
+# Take in a string, concat to it, replace things, capitalize, and count occurances of 's'
+
+# user_input = input("Enter a string: ")
+# user_input = user_input + " Python is cool!"
+# print(user_input)
+# 
+# user_input = user_input.replace("Python", "Java")
+# print(user_input)
+# 
+# user_upper = user_input.upper()
+# user_title = user_input.title()
+# 
+# print(user_upper, user_title)
+# 
+# print(user_input.lower().count('s'))
+
+
+# Problem 2
+# Ask the user for a string, and 2 bounds. Print the slice between those bounds.
+
+# user_string = input("Enter a string: ")
+# 
+# lower_bound = int(input("Please enter a lower slicing bound: "))
+# upper_bound = int(input("Please enter a upper slicing bound: "))
+# print(user_string[lower_bound:upper_bound])
+
+# Inverse of the problem:
+# print(user_string[:lower_bound] + user_string[upper_bound:])
+
+# my_list = list(range(1, 10))
+# print(my_list)
+
+# Range gives a sequence of numbers from the first argument to the second - 1.
+# List converts this into a new datatype 'list'. 
+# We can confirm the datatype by running type on my_list.
