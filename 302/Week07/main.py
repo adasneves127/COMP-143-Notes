8d1816a50a1015518af189b3b56f5b21934584bd
Alex Dasneves <adasneves127@gmail.com>
2023-03-01 10:00:11 -0500

    Added Review

---
+++ 302/Week07/main.py
@@ -0,0 +1,16 @@
+# Midterm Review
+
+# Part 1:
+# False: RAM is used for long-term memory
+# True: The clear() method empties a list and leaves you with []
+# False: The append() method adds an element to the start of a list.
+# Python is an interpreted language
+# False: sequence[-1] returns the first element of a sequence.
+
+# What does computer science study?
+"""
+All things that can be computed.
+"""
+
+# What does the "CPU" do in a computer?
+The 'brain' of the computer
